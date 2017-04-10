0a637efea5d8333f1a18b414a3c6c53615307b80
Matthijs Kamstra <matthijs@fonk.amsterdam>
2017-04-10 21:49:19 +0200

    example loader with vue-resources

---
+++ example/tutorial_loading/readme.md
@@ -0,0 +1,20 @@
+# loading ip example
+
+
+[Read the html source](bin/index.html) for more details
+
+In this example:
+
+- bootstrap for styling
+- css loader
+- get your ip with vue-resources
+
+
+
+Don't forget to add vue-resources to your html
+
+```html
+<!-- vue.js and vue-resources -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.2.6/vue.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/1.3.1/vue-resource.js"></script>
+```
---
+++ example/tutorial_loading/src/Main.hx
@@ -0,0 +1,43 @@
+package ;
+
+import vue.Vue;
+import vue.VueResource;
+
+import js.Lib;
+
+class Main
+{
+	function new()
+	{
+		trace("Haxe Vue.js example");
+
+		var vm = new Vue({
+			el:'#app',
+			data:function () {
+				return {
+					loading : false,
+					message : 'test',
+					ip : 'loading ip'
+				};
+			},
+			mounted:function() {
+				var scope = Lib.nativeThis;
+				scope.loading = true;
+				// GET request
+				VueResource.get('http://ip.jsontest.com/').then(
+					function (response:Response){
+						scope.ip = response.body.ip;
+						scope.loading = false;
+					}, function (error){
+						trace(error);
+					}
+				);
+			}
+		});
+	}
+
+	static public function main()
+	{
+		var main = new Main();
+	}
+}
