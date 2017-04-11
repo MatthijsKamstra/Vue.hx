22461ac79802f65ce5c565a64c48e0ac1edf431f
Matthijs Kamstra <matthijs@fonk.amsterdam>
2017-04-11 09:55:26 +0200

    example with VueComponent

---
+++ example/tutorial04b/src/TodoItem.hx
@@ -0,0 +1,9 @@
+package;
+
+@template("<li>{{ todo.text }}</li>") //The component class will use the first @template for the template
+class TodoItem extends vue.VueComponent {
+
+	inline public function new(){
+		super('todo-item', {props : ['todo']});
+	}
+}
--- src/vue/VueComponent.hx
+++ src/vue/VueComponent.hx
@@ -4,21 +4,20 @@ import haxe.ds.StringMap;
 import vue.Vue;
 
 @template('<p>This is the standard Template</p>')
-class VueComponent 
-{
-    private static var Components:StringMap<VueComponent>;
+class VueComponent {
 
-    public function new(tag:String, options:VueComponentOptions)
-    {
-        if(options.template == null || options.template == ""){
-            options.template = Meta.getType(Type.getClass(this)).template[0];
-        }
-        options.data = data;
-        Vue.component(tag,options);
-    }
-    public function data():VueComponent
-    {
-        return this;
-    }
+	private static var Components:StringMap<VueComponent>;
+
+	public function new(tag:String, options:VueComponentOptions) {
+		if(options.template == null || options.template == ""){
+			options.template = Meta.getType(Type.getClass(this)).template[0];
+		}
+		options.data = data;
+		Vue.component(tag,options);
+	}
+
+	public function data():VueComponent {
+		return this;
+	}
 
 }
